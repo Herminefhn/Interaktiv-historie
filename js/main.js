caf0f8eff59ea28d8857ca2aeeac67f8e96ede30
Herminefhn <143608009+Herminefhn@users.noreply.github.com>
2024-03-08 13:50:55 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+
+function chooseHouse(){
+    document.getElementById("img1").src="img/interaktiv2fixed.jpg"
+    document.getElementById ("scene2").style.display="block"
+    document.getElementById ("story1").style.display="none"
+}
+
+function house1(){
+    document.getElementById("img1").src="img/hus1.jpg"
+    document.getElementById ("scene2").style.display="none"
+    document.getElementById ("story1").style.display="block"
+}
+
+function house1p2(){
+    document.getElementById("img1").src="img/hus1p3.jpg"
+    document.getElementById ("scene2").style.display="none"
+    document.getElementById ("story1").style.display="none"
+    document.getElementById("hus1Choice").style.display="block"
+
+}
+
+function house2(){
+    document.getElementById("img1").src="img/hus2fixed.jpg"
+    document.getElementById ("scene2").style.display="none"
+    document.getElementById("story2").style.display="block"
+    
+}
+
+function house3(){
+    document.getElementById("img1").src="img/hus3.jpg"
+    document.getElementById ("scene2").style.display="none"
+}
+
+function story1(){
+    document.getElementById("img1").src="img/hus1p2.jpg"
+    document.getElementById ("scene2").style.display="none"
+    document.getElementById("hidden3").style.display="block"
+}
+
+function drugs1(){
+    document.getElementById("img1").src="img/hus1slutt1.jpg"
+    document.getElementById ("scene2").style.display="none"
+    document.getElementById("hus1Choice").style.display="none"
+}
+
+function drugs2(){
+    document.getElementById("img1").src="img/hus1slutt2.jpg"
+    document.getElementById ("scene2").style.display="none"
+    document.getElementById("hus1Choice").style.display="none"
+}
+
+function house2b1(){
+    document.getElementById("img1").src="img/hus2slutt1.jpg"
+    document.getElementById ("scene2").style.display="none"
+    document.getElementById("hus1Choice").style.display="none"
+}
+
+
+function house2b2() {
+    location.reload();
+}
